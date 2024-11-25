4e358da9fd6a620f387900f5d333cfbd878b92d4
Abhisheka <20535197@student.curtin.edu.au>
2024-11-25 15:26:32 +0530

    Taking user inputs from gui

--- user/src/main/java/org/example/user/UserApplication.java
+++ user/src/main/java/org/example/user/UserApplication.java
@@ -2,6 +2,7 @@ package org.example.user;
 
 import javafx.application.Application;
 //import org.example.user.gui.App;
+import org.example.user.gui.App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,9 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class, args);
 //        SpringApplication.run(UserApplication.class, args);
-//        Application.launch(App.class, args);
+        SpringApplication.run(UserApplication.class, args);
+        Application.launch(App.class, args);
     }
 
 
--- user/src/main/java/org/example/user/service/Inputs.java
+++ user/src/main/java/org/example/user/service/Inputs.java
@@ -1,6 +1,8 @@
 package org.example.user.service;
 
 import com.example.KeyValueObject;
+import javafx.application.Platform;
+import javafx.scene.control.TextArea;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
@@ -18,56 +18,28 @@ public class Inputs {
     private final RestTemplate restTemplate;
     private final AtomicInteger sendMessageCount = new AtomicInteger(0);
 
-    public Inputs(RestTemplate restTemplate){
+    public Inputs(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-
-        startTimedHelloWorld();
     }
 
-    public void startTimedHelloWorld() {
-        Scanner scanner = new Scanner(System.in);
-        String userChoice;
-
-        System.out.println("Do you want to run timedHelloWorld? (yes/no)");
-        userChoice = scanner.nextLine();
-
-        while (userChoice.equalsIgnoreCase("yes")) {
-            timedHelloWorld();
-            System.out.println("Do you want to run timedHelloWorld again? (yes/no)");
-            userChoice = scanner.nextLine();
-        }
-
-        System.out.println("Program terminated.");
-
-//        ArrayList<Integer> randomSeconds = new ArrayList<>();
-//
-//        while (randomSeconds.size() < 3) {
-//            int num = random.nextInt(60);
-//            if (!randomSeconds.contains(num)) {
-//                randomSeconds.add(num);
-//            }
-//        }
-//
-//        Collections.sort(randomSeconds);
+    public void runTimedHelloWorld(TextArea outputArea){
+        new Thread(() -> {
+            timedHelloWorld(outputArea);
+        }).start();
     }
 
-    public void timedHelloWorld() {
-        Random random = new Random();
+    private void timedHelloWorld(TextArea outputArea) {
+        Random random = new Random(); //ThreadLocalRandom
         AtomicInteger value = new AtomicInteger();
-
         sendMessageCount.set(0);
-
-
 
         ArrayList<Integer> randomSeconds = new ArrayList<>();
         randomSeconds.add(5);
         randomSeconds.add(5);
         randomSeconds.add(5);
 
-        System.out.println("Random seconds (in ascending order): " + randomSeconds);
-
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(randomSeconds.size());
-
+        BlockingQueue<String> messageQueue = new LinkedBlockingQueue<>();
 
         for (int seconds : randomSeconds) {
             int iterationTime = 4 + random.nextInt(2);
@@ -75,20 +47,24 @@ public class Inputs {
             scheduler.schedule(() -> {
                 long printEndTime = System.currentTimeMillis() + iterationTime * 1000;
                 while (System.currentTimeMillis() < printEndTime) {
-//                    System.out.println("Hello world " + seconds + " from thread " + Thread.currentThread().getName());
                     value.set(1 + random.nextInt(20));
 
-                    if(value.get() <= 10){
-                        sendMessage_topic_1to10(new KeyValueObject(
+                    if (value.get() <= 10) {
+                        KeyValueObject keyValueObject = new KeyValueObject(
                                 String.valueOf(System.currentTimeMillis()) + Thread.currentThread().getId(),
                                 value.get(),
                                 1 + random.nextInt(10),
                                 false
-                                ));
-                        System.out.println("Hello world " + seconds + " from thread " + Thread.currentThread().getName());
-
+                        );
+                        sendMessage_topic_1to10(keyValueObject);
+//                        Platform.runLater(() -> outputArea.appendText(
+//                                " from thread " + Thread.currentThread().getName() + "\n" + keyValueObject + "\n"
+//                        ));
+//                        outputArea.appendText(" from thread " + Thread.currentThread().getName() + "\n"
+//                        + keyValueObject + "\n");
+                        messageQueue.add("From thread " + Thread.currentThread().getName() + "\n" + keyValueObject + "\n");
                         sendMessageCount.incrementAndGet();
-                    }else{
+                    } else {
 //                        sendMessage_topic_11to20(new KeyValueObject(
 //                                String.valueOf(System.currentTimeMillis()) + Thread.currentThread().getId(),
 //                                value.get(),
@@ -96,13 +72,10 @@ public class Inputs {
 //                                false));
                     }
 
-
-
-
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
-                        System.err.println("Interrupted during print loop");
+                        Platform.runLater(() -> outputArea.appendText("Interrupted during print loop\n"));
                         Thread.currentThread().interrupt();
                         break;
                     }
@@ -110,12 +83,28 @@ public class Inputs {
             }, seconds, TimeUnit.SECONDS);
         }
 
+        new Thread(() -> {
+            String finalMessage = null;
+            while (true) {
+                try {
+                    finalMessage = messageQueue.take();
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                String finalMessage1 = finalMessage;
+                outputArea.appendText(finalMessage1);
+//                Platform.runLater(() -> outputArea.appendText(finalMessage1));
+
+            }
+        }).start();
+
         scheduler.shutdown();
         try {
             scheduler.awaitTermination(20, TimeUnit.SECONDS);
-            System.out.println("sendMessage was called " + sendMessageCount.get() + " times.");
+            outputArea.appendText("sendMessage was called " + sendMessageCount.get() + " times.\n");
         } catch (InterruptedException e) {
-            System.err.println("Scheduler interrupted during awaitTermination.");
+            outputArea.appendText("Scheduler interrupted during awaitTermination.\n");
         }
     }
 
@@ -131,7 +120,7 @@ public class Inputs {
         try {
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
             if (response.getStatusCode().is2xxSuccessful()) {
-                System.out.println("Message sent successfully: " + response.getBody());
+
             } else {
                 System.out.println("Failed to send message. Status: " + response.getStatusCode());
             }
@@ -151,7 +140,7 @@ public class Inputs {
         try {
             ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
             if (response.getStatusCode().is2xxSuccessful()) {
-                System.out.println("Message sent successfully: " + response.getBody());
+
             } else {
                 System.out.println("Failed to send message. Status: " + response.getStatusCode());
             }
